2b8b19666337149f2cda7afca3cafc15011c59ac
sambhavi khare <sambhavikhare1501@gmail.com>
2026-05-20 18:18:31 +0530

    Add difficulty selection to Vocabloom game

--- Vocabloom.py
+++ Vocabloom.py
@@ -52,27 +52,68 @@ def show_meaning(word):
 
 # MAIN GAME LOOP
 while True:
+        #difficulty:
+    # DIFFICULTY SELECTION LOOP
+    while True:
 
-    # generate valid 5-letter word
+        print(Fore.CYAN + "\nChoose Difficulty:\n")
+
+        print("1. EASY\n")
+        print("2. MEDIUM\n")
+        print("3. HARD\n")
+
+        difficulty = input(
+            Fore.YELLOW
+            + "Enter your choice: (1/2/3) "
+        )
+
+        # EASY
+        if difficulty == "1":
+
+            word_length = 5
+            attempts = 10
+            break
+
+        # MEDIUM
+        elif difficulty == "2":
+
+            word_length = 6
+            attempts = 8
+            break
+
+        # HARD
+        elif difficulty == "3":
+
+            word_length = 7
+            attempts = 6
+            break
+
+        # INVALID
+        else:
+
+            print(
+                Fore.RED
+                + "\nInvalid input! Please enter a valid input (1/2/3)\n"
+            )
+        
+    # generate valid word
     while True:
 
         secret_word = r.word(
-            word_min_length=5,
-            word_max_length=5
+            word_min_length=word_length,
+            word_max_length=word_length
         ).lower()
 
         if (
             secret_word in web2lowerset
             and secret_word.isalpha()
-            and len(secret_word) == 5
+            and len(secret_word) == word_length
         ):
             break
 
-    attempts = 10
 
     print(Fore.MAGENTA + "\n🌸 Welcome to Vocabloom <3 🌸")
-    print(Fore.CYAN + "Be CAREFULL!!!! You Have Only 10 Guesses\n")
-
+    print(Fore.CYAN + f"\nBe CAREFULL!!!! You Have Only {attempts} Guesses")
     # GAME ROUND LOOP
     while attempts > 0:
 
@@ -80,8 +121,8 @@ while True:
         guess = input("enter your guess: ").lower()
 
         # checking length
-        if len(guess) != 5:
-            print(Fore.RED + "\nPlease Enter Exactly 5 Letters\n")
+        if len(guess) != word_length:
+            print(Fore.RED + f"\nPlease Enter Exactly {word_length} Letters")
             continue
 
         # check alphabets only
@@ -93,7 +134,7 @@ while True:
         if guess not in web2lowerset:
             print(
                 Fore.RED
-                + "\nErr... I don't know what that word is :/\nMaybe try some valid english word\n"
+                + "\nErr... I don't know what that word is :/Maybe try some valid english word\n"
             )
             continue
 
@@ -115,7 +156,7 @@ while True:
         print("\nResult: ", end="")
 
         # checking letters
-        for i in range(5):
+        for i in range(word_length):
 
             # correct letter + correct position
             if guess[i] == secret_word[i]:
